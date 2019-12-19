7c16d09cd943f2cf6f117228b52ee6414d3de4bb
7emini <touchmain@qq.com>
2019-12-19 21:23:05 +0800

    修改配置文件

--- Base/Conf.php
+++ Base/Conf.php
@@ -3,7 +3,7 @@ return array(
     //数据库配置
     'db_host'   =>  "127.0.0.1",
     'db_user'   =>  "root",
-    'db_pwd'    =>  "root",
+    'db_pwd'    =>  "12345",
     'db_name'   =>  "mybook",
     'db_port'   =>  "3306",
     'charset'   =>  "utf8",
